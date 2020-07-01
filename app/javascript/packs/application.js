b3bfe2436710d6073c4d88018718cb209cffbb7f
Mikey Ettlinger <mikeyettlinger@gmail.com>
2020-07-01 12:37:59 +0100

    graph

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,23 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+
+
+import { initApex } from '../plugins/init_apex';
+
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+
+document.addEventListener('turbolinks:load', () => {
+  initApex();
+  // Call your functions here, e.g:
+  // initSelect2();
+});
